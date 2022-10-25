0b64fc0ef74c77bfa344cad2fb995c3b6d66dcb3
hamburger <tik.shitting@gmail.com>
2022-10-25 17:56:16 +0300

    Commit message

--- custom-search.js
+++ custom-search.js
@@ -5,17 +5,21 @@ let btn = document.querySelector('input[name="search-btn"]');
 let clearBtn = document.querySelector('input[name="clear-btn"]');
 let result = document.querySelector('.search-result');
 
-let articleList = JSON.parse(require('./article-list.json'));
-console.log(articleList);
 
 searchBar.addEventListener('input', searcher);
 clearBtn.addEventListener('click', () => { searchBar.value = '' });
 btn.addEventListener('click', searcher);
 
+
+
 async function searcher() {
     query = searchBar.value.toLowerCase()
+
+    let articleArray = await getJsonData();
+    console.log(articleArray);
+
     for (let i = 1; i < 2; i++) {
-        let url = 'https://yawgoo87.github.io/custom-static-search/articles/'+i
+        let url = 'https://yawgoo87.github.io/custom-static-search/articles/'+i;
         let b;
         let text
 
@@ -34,15 +38,9 @@ async function pageRequest(url){
     return doc;
 }
 
-
-// async function pageRequest(url){
-//     const response = await fetch(url)
-//         .then((res) => res.text())
-//         .then((html) => {
-//             var parser = new DOMParser();
-// 	        var doc = parser.parseFromString(html, 'text/html');
-//         })
-//         .then((doc) => { return doc })
-//         .catch((err) => console.log(err));
-//     return response;
-// }
+async function getJsonData(){
+    let jsonUrl = 'https://yawgoo87.github.io/custom-static-search/article-list.json';
+    const response = await fetch(jsonUrl);
+    const json = response.json();
+    return json;
+}
